255188ef03414189b0b3ca53bc2fb1cb2db29a9c
salmamouri <mouriclub@gmail.com>
2022-10-25 20:56:07 +0600

    second commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBO54TieRECXimyKtzUyYhSyBQyqkp1cSw",
+  authDomain: "tech-learn-ceb3d.firebaseapp.com",
+  projectId: "tech-learn-ceb3d",
+  storageBucket: "tech-learn-ceb3d.appspot.com",
+  messagingSenderId: "1050911940194",
+  appId: "1:1050911940194:web:5e9801c2456e152776fb3b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
