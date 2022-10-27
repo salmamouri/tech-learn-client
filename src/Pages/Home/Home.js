e2d19f926eff9bbcfd0c64589701af653e9e9f67
salmamouri <mouriclub@gmail.com>
2022-10-27 23:42:20 +0600

    header added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 const Home = () => {
   return (
-    <div>
-      <h3>This home</h3>
-    </div>
+    <Container className="bg-primary m-5 p-5 mx-auto">
+      <h3 className="text-center text-warning fs-1 fw-bold">Tech Learn</h3>
+      <p className="text-center text-light fs-3 fw-semoibold">
+        Choose Tech Learn as Your Techy Learning Partner
+      </p>
+    </Container>
   );
 };
 
