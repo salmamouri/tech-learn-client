7f854ec0a500d65ea60615d6a4e7f663c3c6b6c2
salmamouri <mouriclub@gmail.com>
2022-10-28 18:49:10 +0600

    footer & loading added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 
 const Home = () => {
   return (
-    <Container className="bg-primary m-5 p-5 mx-auto">
+    <Container className="bg-primary m-5 p-5 mx-auto mt-5">
       <h3 className="text-center text-warning fs-1 fw-bold">Tech Learn</h3>
       <p className="text-center text-light fs-3 fw-semoibold">
         Choose Tech Learn as Your Techy Learning Partner
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h3>Footer</h3>
+    <div className="bg-info  p-4 mt-5">
+      <h5 className="text-warning text-center fs-4">Tech Learn</h5>
+      <h4 className=" text-primary text-center">Be Creative in Learning</h4>
+      <p className="text-warning text-center fw-bold">&copy; 2022</p>
     </div>
   );
 };
