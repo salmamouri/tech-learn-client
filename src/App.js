3d0792db96045abfebcde3114ba420b3ebbb267b
salmamouri <mouriclub@gmail.com>
2022-10-28 16:35:29 +0600

    darkmode added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { routes } from "./Routes/Routes";
+import "./../src/darkMode.css";
+import { FaMoon } from "react-icons/fa";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <div className="">
+    <div className={`App ${theme}`}>
+      <FaMoon className="mx-5 border-rounded" onClick={toggleTheme} />
+
       <RouterProvider router={routes}></RouterProvider>
     </div>
   );
